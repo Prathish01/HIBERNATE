16f77c8894653741e88f2f57b5a720d7b3471dd0
Prathish S <prathish472@gmail.com>
2026-02-19 19:21:50 +0530

    To do insert, update and delete.

---
+++ PROJECT 1/Student.java
@@ -0,0 +1,63 @@
+package com.jspiders.hibernate;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "student")
+public class Student 
+{
+
+    @Id
+    private int rollno;
+    private String name;
+    private int age;
+    private String contact;
+
+    public int getRollno() {
+        return rollno;
+    }
+
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+   
+    public Student() {}
+
+    public Student(int rollno, String name, int age, String contact) {
+        this.rollno = rollno;
+        this.name = name;
+        this.age = age;
+        this.contact = contact;
+    }
+
+    // getters & setters
+}
