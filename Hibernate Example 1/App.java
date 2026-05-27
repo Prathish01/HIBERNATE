717d6680252087750c120c70fb5823b7ab77dbee
Prathish S <prathish472@gmail.com>
2026-05-27 18:11:33 +0530

    Update App.java

--- Hibernate Example 1/App.java
+++ Hibernate Example 1/App.java
@@ -5,11 +5,15 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+
 /**
  * Hello world!
  */
-public class App {
-    public static void main(String[] args) {
+
+public class App 
+{
+    public static void main(String[] args) 
+    {
         EntityManagerFactory emf= Persistence.createEntityManagerFactory("prathish");
         EntityManager em=emf.createEntityManager();
         EntityTransaction et=em.getTransaction();
