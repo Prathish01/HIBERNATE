ec09b3c673f910e52ae44120b3ccf5e734649822
Prathish S <prathish472@gmail.com>
2026-05-27 18:02:33 +0530

    Update productdto.java

--- FINAL PROJECT/Dto files/productdto.java
+++ FINAL PROJECT/Dto files/productdto.java
@@ -3,13 +3,17 @@ package com.hibernate.dao;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
+
 @Entity
 public class products 
+	
 {
 @Id
 private int productid;
 private String productname;
+	
 public int getProductid() {
+	
 	return productid;
 }
 public void setProductid(int productid) {
