2c3f57fa349653581ab4ebdcded94db3ad6ef43c
Prathish S <prathish472@gmail.com>
2026-05-27 17:55:24 +0530

    Update delivery.java

--- FINAL PROJECT/delivery.java
+++ FINAL PROJECT/delivery.java
@@ -3,15 +3,20 @@ package com.hibernate.dao;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
+
 @Entity
 public class Delivery 
 {
+	
 @Id
 
 private int Deliveryid;
+	
 private String type;
 private String status;
+	
 public int getDeliveryid() {
+	
 	return Deliveryid;
 }
 public void setDeliveryid(int deliveryid) {
