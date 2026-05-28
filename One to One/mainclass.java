e8ce11a621abca5b0142096522f78c838244619c
Prathish S <prathish472@gmail.com>
2026-05-28 22:09:56 +0530

    Update mainclass.java

--- One to One/mainclass.java
+++ One to One/mainclass.java
@@ -7,11 +7,13 @@ import com.hibernate.dto.studentdto;
  * Hello world!
  */
 public class App {
+    
     public static void main(String[] args) {
 //        idcarddto.insertdata(1, 102, "20-12-2020");
 //        studentdto.insertdata(1, "22", "male", 1);
 //    	idcarddto.updatedata(1, 23, "23-jun-2019");
 //    	studentdto.updatedata(1, "24", "female", 1);
+        
 //    	studentdto.readdata(1);
 //    	idcarddto.readdata(1);
 //    	idcarddto.insertdata(2, 202, "15-1-2017");
