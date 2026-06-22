5690b23930d7caaba42e9b163ef20133753c2007
Prathish S <prathish472@gmail.com>
2026-06-22 18:03:24 +0530

    Update FetchAllData.java

--- Hibernate Example 1/FetchAllData.java
+++ Hibernate Example 1/FetchAllData.java
@@ -7,8 +7,11 @@ import javax.persistence.EntityManagerFactory;
 
 import javax.persistence.EntityTransaction;
 
+
 import javax.persistence.Persistence;
+
 import javax.persistence.Query;
+
 
 public class FetchAllData {
 
