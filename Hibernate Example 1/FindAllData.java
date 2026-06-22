cf9782e8fd4848f0c5091c3103831a58c062ac23
Prathish S <prathish472@gmail.com>
2026-06-22 18:03:41 +0530

    Update FindAllData.java

--- Hibernate Example 1/FindAllData.java
+++ Hibernate Example 1/FindAllData.java
@@ -1,12 +1,19 @@
 package com.jsp.hibernate_first;
 
+
 import javax.persistence.EntityManager;
+
+
 
 import javax.persistence.EntityManagerFactory;
 
+
+
 import javax.persistence.EntityTransaction;
 
+
 import javax.persistence.Persistence;
+
 
 public class FindData {
 
