e6e957037b94f95cd31e26bd6c9f5b5eb3655b2d
Prathish S <prathish472@gmail.com>
2026-06-24 18:31:59 +0530

    Update Adharcard.java

--- PROJECT 3/com.hibernate.Dto/Adharcard.java
+++ PROJECT 3/com.hibernate.Dto/Adharcard.java
@@ -2,9 +2,13 @@ package com.hibernate.dto;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+
 @Entity
+	
 public class Adharcard {
+	
 @Id
+	
 	private int AdharId;
 	private long Adharnumber;
 	private String name;
