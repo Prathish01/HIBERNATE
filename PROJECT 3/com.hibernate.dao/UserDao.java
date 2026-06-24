2d656413322e35a3f13bc0600c137d279238b4d5
Prathish S <prathish472@gmail.com>
2026-06-24 18:31:19 +0530

    Update UserDao.java

--- PROJECT 3/com.hibernate.dao/UserDao.java
+++ PROJECT 3/com.hibernate.dao/UserDao.java
@@ -3,16 +3,20 @@ package com.hibernate.dao;
 import com.hibernate.dto.Adharcard;
 import com.hibernate.dto.User;
 
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+
+
 public class UserDao {
     static EntityManagerFactory factory = Persistence.createEntityManagerFactory("development");
     static EntityManager manager = factory.createEntityManager();
     static EntityTransaction transaction =manager.getTransaction();
      public static void insertUser(int userId, String username,String dateofbirth, String address,String email, long contact)
      {
+		 
     	 User user=new User();
     	 user.setUserId(userId);
     	 user.setUsername(username);
@@ -24,6 +28,7 @@ public class UserDao {
     	 transaction.begin();
     	 manager.persist(user);
     	 transaction.commit();
+		 
    
      }
      public static void findUser(int userId) {
