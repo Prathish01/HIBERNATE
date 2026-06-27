ed03401bc0c65ec07125428c2be977da56d28fdc
Prathish S <prathish472@gmail.com>
2026-06-27 17:01:52 +0530

    Update Main.java

--- PROJECT 1/Main.java
+++ PROJECT 1/Main.java
@@ -6,20 +6,29 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
+
 public class Mainclas 
+	
 {
+	
 public static void main(String [] args)
+	
 {
+	
 	EntityManagerFactory factory =Persistence.createEntityManagerFactory("development");
 	EntityManager manager = factory.createEntityManager();
+	
 	EntityTransaction transaction = manager.getTransaction();
 	Student s2 = new Student();
+	
 	s2.setRollno(2);
 	s2.setName("suresh");
 	s2.setContact("0123456789");
+	
 	s2.setAge(24);
 	transaction.begin();
 	manager.persist(s2);
+	
 	transaction.commit();
 
 }
