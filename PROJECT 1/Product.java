bebf1737d44f2d546d30b4e06aaa49747b24e6d2
Prathish S <prathish472@gmail.com>
2026-06-27 17:02:48 +0530

    Update Product.java

--- PROJECT 1/Product.java
+++ PROJECT 1/Product.java
@@ -3,36 +3,55 @@ package com.jspiders.hibernate.dto;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
+
 @Entity
+	
 public class Product {
+	
      @Id
+	
 	private int productId;
   private String productName;
   private String productPrice;
+	
   private String productType;
+	
   public int getProductId() {
   return productId;
 }
-  public String getProductName() {
+  public String getProductName() 
+	{
+		
 	return productName;
   }
-  public void setProductName(String productName) {
+  public void setProductName(String productName) 
+	{
 	this.productName = productName;
+		
   }
-  public String getProductPrice() {
+  public String getProductPrice()
+	{
 	return productPrice;
+		
   }
-  public void setProductPrice(String productPrice) {
+  public void setProductPrice(String productPrice)
+	{
 	this.productPrice = productPrice;
+		
   }
-  public String getProductType() {
+  public String getProductType()
+	{
 	return productType;
+		
   }
  
-  public void setProductId(int productId) {
+  public void setProductId(int productId) 
+	{
 	this.productId = productId;
+		
   }
-  public void setProductType(String type) {
+  public void setProductType(String type) 
+	{
 	// TODO Auto-generated method stub
 	
   }
