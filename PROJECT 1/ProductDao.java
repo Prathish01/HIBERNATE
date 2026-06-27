5b25786693ba6160fd5b27e373e5f51679e134b9
Prathish S <prathish472@gmail.com>
2026-06-27 17:03:18 +0530

    Update ProductDao.java

--- PROJECT 1/ProductDao.java
+++ PROJECT 1/ProductDao.java
@@ -5,17 +5,23 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
-public class ProductDao {
+public class ProductDao 
+{
    public static void insertProduct(int productId,String name, String price,String type) {
 	   Product product=new Product();
+	   
        product.setProductId(productId);
+	   
        product.setProductName(name);
        product.setProductType(type);
+	   
        product.setProductPrice(price);
        
 	   EntityManagerFactory factory=Persistence.createEntityManagerFactory("devolopment");
+	   
 	   EntityManager manager = factory.createEntityManager();
 	   EntityTransaction transaction=manager.getTransaction();
+	   
 	   transaction.begin();
 	   manager.persist(product);
 	   transaction.commit();
